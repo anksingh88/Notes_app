3149929e0a7cda05af58823423b14bb23ad2becb
anksingh88 <88anksingh@gmail.com>
2021-07-08 00:53:36 +0530

    update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ addBtn.addEventListener("click", function(e) {
   notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
-  addtitle.value = "";
+  addTitle.value = "";
 //   console.log(notesObj);
   showNotes();
 });
